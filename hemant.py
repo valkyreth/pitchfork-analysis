911943ba3ecb159c202bba6030962edf2f7ca900
Meraj <zxanfly@gmail.com>
2018-10-11 22:43:27 +0530

    changed hemant.py

--- hemant.py
+++ hemant.py
@@ -1,5 +1,5 @@
 import pandas as pd
-import database_connection as db
+#import database_connection as db
 import matplotlib.pyplot as plt
 import pickle
 from sklearn.feature_extraction.text import TfidfVectorizer
@@ -10,11 +10,13 @@ from sklearn.ensemble import RandomForestClassifier
 from sklearn.svm import LinearSVC
 import seaborn as sns
 from sklearn.model_selection import cross_val_score
+import sqlite3
 
-# c, conn = db.connection()
-# data = pd.read_sql_query("select r.score, c.content from reviews r, content c where r.reviewid = c.reviewid ", conn)
-# data.to_pickle("./reviews.pkl")
-# db.close_db(c, conn)
+conn = sqlite3.connect('pitchfork.sqlite')
+data = pd.read_sql_query("select r.score, c.content from reviews r, content c where r.reviewid = c.reviewid ", conn)
+data.to_pickle("./reviews.pkl")
+#db.close_db(c, conn)
+conn.close()
 
 data = pd.read_pickle("./reviews.pkl")
 data['rounded'] = [round(score) for score in data.score]
@@ -44,30 +46,30 @@ models = [
 
 
 def training(x, y, model):
-    # lr = model
-    # lr.fit(x, y)
-    # with open(model.__class__.__name__, 'wb') as file:
-    #     pickle.dump(lr, file)
+    lr = model
+    lr.fit(x, y)
     with open(model.__class__.__name__, 'wb') as file:
-        lr = pickle.load(file)
+        pickle.dump(lr, file)
+    # with open(model.__class__.__name__, 'wb') as file:
+    #     lr = pickle.load(file)
     return lr
 
 
 for i in models:
     model = training(X_train, y_train, i)
-    print(i.__class__.__name__ + " : " + model.score(X_test, y_test))
+    print(i.__class__.__name__ + " : " + str(model.score(X_test, y_test)))
 
 
-# CV = 5
-# cv_df = pd.DataFrame(index=range(CV * len(models)))
-# entries = []
-# for model in models:
-#     model_name = model.__class__.__name__
-#     accuracies = cross_val_score(model, X_train, y_train, scoring='accuracy', cv=CV)
-#     for fold_idx, accuracy in enumerate(accuracies):
-#         entries.append((model_name, fold_idx, accuracy))
-# cv_df = pd.DataFrame(entries, columns=['model_name', 'fold_idx', 'accuracy'])
-# sns.boxplot(x='model_name', y='accuracy', data=cv_df)
-# sns.stripplot(x='model_name', y='accuracy', data=cv_df,
-#               size=8, jitter=True, edgecolor="gray", linewidth=2)
-# plt.show()
+CV = 5
+cv_df = pd.DataFrame(index=range(CV * len(models)))
+entries = []
+for model in models:
+    model_name = model.__class__.__name__
+    accuracies = cross_val_score(model, X_train, y_train, scoring='accuracy', cv=CV)
+    for fold_idx, accuracy in enumerate(accuracies):
+        entries.append((model_name, fold_idx, accuracy))
+cv_df = pd.DataFrame(entries, columns=['model_name', 'fold_idx', 'accuracy'])
+sns.boxplot(x='model_name', y='accuracy', data=cv_df)
+sns.stripplot(x='model_name', y='accuracy', data=cv_df,
+              size=8, jitter=True, edgecolor="gray", linewidth=2)
+plt.show()
